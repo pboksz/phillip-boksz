d526770cd2cecc9cb293bf0f2f811af8e19129ec
Phillip Boksz <pboksz@gmail.com>
2013-07-18 10:39:36 +0200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,8 @@ module PhillipBokszCom
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # for fixing weird Heroku issues
+    config.assets.initialize_on_precompile = false
   end
 end
