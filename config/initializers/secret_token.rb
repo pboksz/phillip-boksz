5434290e9411c9ce7c8d98e079edb7485a21c67b
Phillip Boksz <phillip@lunarlogic.io>
2015-01-12 11:34:04 +0100

    Move secrets to secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PhillipBokszCom::Application.config.secret_token = '0ab0b56cc70e4452957dd68ca160a604b56066f62596bdbcd8fe1ab958082de85a1c181b10ba98b48507319d25c73af34cd0ae6525bcefe588d430d83a079d8f'
