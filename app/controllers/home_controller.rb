0f3451d52df247c2b60f5c2f7c91311da9af3484
Phillip Boksz <phillip@lunarlogic.io>
2015-01-19 13:08:59 +0100

    Use ruby 1.9 hash notation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def index
-    render :index, :locals => {
-      :name => 'Phillip Boksz',
-      :gravatar_url => "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest('pboksz@gmail.com')}?s=200",
-      :github_url => 'http://www.github.com/pboksz',
-      :linkedin_url => 'http://www.linkedin.com/in/pboksz',
-      :email => 'pboksz@gmail.com',
-      :lunar_logic_url => 'http://www.lunarlogic.io'
+    render :index, locals: {
+      name: 'Phillip Boksz',
+      gravatar_url: "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest('pboksz@gmail.com')}?s=200",
+      github_url: 'http://www.github.com/pboksz',
+      linkedin_url: 'http://www.linkedin.com/in/pboksz',
+      email: 'pboksz@gmail.com',
+      lunar_logic_url: 'http://www.lunarlogic.io'
     }
   end
 end
