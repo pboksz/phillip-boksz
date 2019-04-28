b964f788567fe46e412168e49bfb6c374e30cf05
Phillip Boksz <pboksz@gmail.com>
2019-04-28 22:41:00 +0100

    Update icons usage for font awesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,20 @@
 module ApplicationHelper
-  def icon_link(link, icon_name, options = {})
+  def icon_link(link, icon_type, icon_name, options = {})
     attributes = options[:blank] ? { target: '_blank' } : {}
-    link_to(link, attributes) { icon(icon_name) }
+    link_to(link, attributes) { icon(icon_type, icon_name) }
   end
 
   def star_rating_tag(stars)
-    return 5.times.map { icon("question-circle") }.join.html_safe if stars.to_f == 0
+    return 5.times.map { icon('fas', 'question-circle') }.join.html_safe if stars.to_f == 0
 
     empty = (5 - stars).to_i
     times = stars.to_i
     half = (stars % 1) == 0.5
 
     icons = ''
-    times.times { icons += icon('star') }
-    icons += icon('star-half-o') if half
-    empty.times { icons += icon('star-o') }
+    times.times { icons += icon('fas', 'star') }
+    icons += icon('fas', 'star-half-alt') if half
+    empty.times { icons += icon('far', 'star') }
 
     icons.html_safe
   end
@@ -22,7 +22,7 @@ module ApplicationHelper
   def switch_project_tag(project, type)
     if project.present? && type
       link_to "##{project}", class: type do
-        icon(get_icon_from_type(type))
+        icon('fas', get_icon_from_type(type))
       end
     end
   end
