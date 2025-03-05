1efd13727c6040f44ea2eba3e6b3cd722e933820
Phillip Boksz <pboksz@gmail.com>
2025-03-05 10:10:38 +0000

    Add ability for GamesRepository to retry call when bgg is loading games

--- app/repositories/games_repository.rb
+++ app/repositories/games_repository.rb
@@ -1,6 +1,12 @@
 class GamesRepository
   BGG_COLLECTION_URL = "https://www.boardgamegeek.com/xmlapi2/collection?username=deadumms&own=1&stats=1&excludesubtype=boardgameexpansion"
   BGG_GAME_URL = "https://boardgamegeek.com/boardgame/"
+  RETRY_LIMIT = 3
+  SLEEP_AMOUNT = 10
+
+  def initialize
+    @retries = 0
+  end
 
   def all
     parsed_games
@@ -20,8 +26,22 @@ class GamesRepository
     Hash.from_xml(response).dig("items", "item")
   end
 
+  def load_games
+    raise "loading games" if bgg_games.blank?
+    bgg_games
+  rescue
+    return unless retry?
+    sleep(SLEEP_AMOUNT)
+    retry
+  end
+
+  def retry?
+    @retries += 1
+    @retries <= RETRY_LIMIT
+  end
+
   def parsed_games
-    @parsed_games ||= bgg_games.map { |item| game_object(item) }.compact
+    @parsed_games ||= load_games.map { |item| game_object(item) }.compact
   end
 
   def all_data_present?(item)
